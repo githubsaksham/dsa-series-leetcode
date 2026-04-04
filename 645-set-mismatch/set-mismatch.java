689a4c0ec8443677ea6236da8333858a2b6bc0c7
Saksham Agarwal <131918063+githubsaksham@users.noreply.github.com>
2026-04-04 11:57:39 +0530

    Time: 14 ms (34.31%) | Memory: 47.5 MB (83.75%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,25 +1,55 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int len = nums.length;
-        HashSet<Integer> data = new HashSet<>();
-        int run =1;
+        // int len = nums.length;
+        // HashSet<Integer> data = new HashSet<>();
+        // int run =1;
 
-        int[] result = new int[2];
-        for(int i=0;i<len;i++){
-            if(data.contains(nums[i]))
-               result[0] = nums[i];  
+        // int[] result = new int[2];
+        // for(int i=0;i<len;i++){
+        //     if(data.contains(nums[i]))
+        //        result[0] = nums[i];  
 
-            data.add(nums[i]);
-        }
+        //     data.add(nums[i]);
+        // }
 
-        for(int i=1;i<=len;i++){
-            if(!data.contains(i)){
-                result[1] = i;
-            }
-        }
+        // for(int i=1;i<=len;i++){
+        //     if(!data.contains(i)){
+        //         result[1] = i;
+        //     }
+        // }
 
 
-        return result;
+        // return result;
+
+           int len = nums.length ;
+           int ArrSum = 0;
+           int realSum = len*(len+1)/2;
+           HashSet<Integer> data = new HashSet<>();
+           int[] result = new int[2];
+
+           for(int i=0;i<len;i++){
+                if(data.contains(nums[i]))
+                      result[0] = nums[i];
+
+                data.add(nums[i]);
+                ArrSum+=nums[i];
+           }
+           result[1] = result[0] + (realSum - ArrSum);
+
+           return result;
+
+
+           
+
+
+
+
+
+
+
+
+
+        
         
         
     }
