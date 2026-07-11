5b20a98c6f76d00a85b1f7bac26d228936e723bb
Saksham Agarwal <131918063+githubsaksham@users.noreply.github.com>
2026-07-11 11:01:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (38.09%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -5,7 +5,7 @@ public:
         long long rev =0;
        if (x < 0 || (x % 10 == 0 && x != 0))
             return false;
-        while(rev<x){
+        while(num!=0){
             int digit = num%10;
             rev = rev*10+digit;
             num = num/10;
