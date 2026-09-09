5142eba84d9178876ef91c839599f5de8fc961d7
Saksham Agarwal <131918063+githubsaksham@users.noreply.github.com>
2026-09-09 23:47:57 +0530

    Time: 1 ms (100.00%) | Memory: 48.8 MB (70.93%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -0,0 +1,51 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        
+        // Binary search smaller array
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int n = nums1.length;
+        int m = nums2.length;
+
+        int low = 0;
+        int high = n;
+
+        int totalLeft = (n + m + 1) / 2;
+
+        while (low <= high) {
+            int cut1 = low + (high - low) / 2;
+            int cut2 = totalLeft - cut1;
+
+            int left1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            int left2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+
+            int right1 = (cut1 == n) ? Integer.MAX_VALUE : nums1[cut1];
+            int right2 = (cut2 == m) ? Integer.MAX_VALUE : nums2[cut2];
+
+            // Correct partition
+            if (left1 <= right2 && left2 <= right1) {
+
+                // Odd total length
+                if ((n + m) % 2 == 1) {
+                    return Math.max(left1, left2);
+                }
+
+                // Even total length
+                return (Math.max(left1, left2)
+                        + (long) Math.min(right1, right2)) / 2.0;
+            }
+
+            // nums1's left part is too large
+            if (left1 > right2) {
+                high = cut1 - 1;
+            } else {
+                // nums1's left part is too small
+                low = cut1 + 1;
+            }
+        }
+
+        return 0.0;
+    }
+}
